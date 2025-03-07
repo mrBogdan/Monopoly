1ebd455c96e8e74d3300f0e33e3377c86c0bed23
Bogdan Babitskiy <dandan250899@gmail.com>
2025-03-08 00:40:10 +0200

    MNP-6: add ws server, fixes

--- service/src/tile/tiles/Cars/audi.ts
+++ service/src/tile/tiles/Cars/audi.ts
@@ -1,8 +1,27 @@
 import { MonopolyTile } from '../../MonopolyTile';
 import { QuadMonopolyReward } from '../../QuadMonopolyReward';
+import { TileType } from '../../TileType';
+import { RewardType } from '../../RewardType';
+import { Monopoly } from '../../Monopoly';
 
 export const audi: MonopolyTile<QuadMonopolyReward> = {
   id: 'audi',
-  name: 'AudiCarTile',
-
+  name: 'Audi',
+  description: 'Audi description',
+  iconUrl: 'adidas.svg',
+  cost: 2000,
+  monopoly: Monopoly.Cars,
+  mortgageCost: 1000,
+  unmortgageCost: 1200,
+  type: TileType.Monopoly,
+  reward: {
+    type: RewardType.QuadMonopolyReward,
+    rent: 250,
+    upgradeRent: {
+      level1Rent: 250,
+      level2Rent: 500,
+      level3Rent: 1000,
+      level4Rent: 2000,
+    }
+  }
 }
