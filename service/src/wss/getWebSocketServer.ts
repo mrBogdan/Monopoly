43b28f5e011949376edd9257ed39d5e37b4d97aa
Bohdan Babytskyi <bbabitskiy@playtika.com>
2025-03-14 11:59:41 +0200

    No-ticket: refactor

--- service/src/wss/getWebSocketServer.ts
+++ service/src/wss/getWebSocketServer.ts
@@ -5,22 +5,20 @@ import { getMessageHandler } from './getMessageHandler';
 let wss: WebSocketServer | null = null;
 
 export const getWebSocketServer = (): WebSocketServer => {
-    if (wss) {
-        return wss;
+    if (!wss) {
+        wss = new WebSocketServer({noServer: true});
+        wss.on('error', console.error);
+
+        wss.on('connection', (ws) => {
+            ws.on('error', console.error);
+
+            ws.on('message', getMessageHandler(ws));
+        });
+
+        wss.on('close', () => {
+            console.log('Client disconnected');
+        });
     }
-
-    wss = new WebSocketServer({noServer: true});
-    wss.on('error', console.error);
-
-    wss.on('connection', (ws) => {
-        ws.on('error', console.error);
-
-        ws.on('message', getMessageHandler(ws));
-    });
-
-    wss.on('close', () => {
-        console.log('Client disconnected');
-    });
 
     return wss;
 };
