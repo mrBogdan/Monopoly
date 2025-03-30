e888ce276eb86ebfcc9a97b53ac9590683af62c6
Bohdan Babytskyi <bbabitskiy@playtika.com>
2025-03-30 12:57:06 +0300

    No-ticket: add code for injectable decorator

--- service/src/di/Injectable.ts
+++ service/src/di/Injectable.ts
@@ -1,6 +1,8 @@
+import 'reflect-metadata';
+
 export function Injectable() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return function (target: object) {
-    // do something with `target`, for example decorate it with some metadata
+    const paramTypes = Reflect.getMetadata('design:paramtypes', target) || [];
+    Reflect.defineMetadata('design:paramtypes', paramTypes, target);
   }
 }
