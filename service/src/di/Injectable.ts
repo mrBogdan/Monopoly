7976ca11207568fd895e5aee7323de515a8eb4cc
Bohdan Babytskyi <bbabitskiy@playtika.com>
2025-03-30 12:57:25 +0300

    No-ticket: add return type for decorator

--- service/src/di/Injectable.ts
+++ service/src/di/Injectable.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 
-export function Injectable() {
+export function Injectable(): ClassDecorator {
   return function (target: object) {
     const paramTypes = Reflect.getMetadata('design:paramtypes', target) || [];
     Reflect.defineMetadata('design:paramtypes', paramTypes, target);
