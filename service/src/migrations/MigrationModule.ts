98621a6b022ec65ba52a3d0c7dc4b79a553cdfdc
Bohdan Babytskyi <bbabitskiy@playtika.com>
2025-04-02 22:31:18 +0300

    No-ticket: don't run migration without needs

--- service/src/migrations/MigrationModule.ts
+++ service/src/migrations/MigrationModule.ts
@@ -1,16 +1,22 @@
 import { Module } from '../decorators/Module';
 import { Client } from 'pg';
 import { migrate } from './migration';
+import { ServiceConfiguration } from '../config/ServiceConfiguration';
+import { CONFIG } from '../config/ConfigService';
 
 export const Migration = Symbol('migration');
 
 @Module({
   services: [{
     param: Migration,
-    useFactory: async (db: Client) => {
+    useFactory: async (db: Client, config: ServiceConfiguration) => {
+      if (!config.withMigration) {
+        return;
+      }
+
       await migrate(db);
     },
-    inject: [Client],
+    inject: [Client, CONFIG],
   }]
 })
 export class MigrationModule {}
