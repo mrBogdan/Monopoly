56678172c7c24ee61294d5f83082e0114f2b52ac
Bohdan Babytskyi <bbabitskiy@playtika.com>
2025-04-14 00:15:37 +0300

    MNP-5: remove duplication

--- service/src/__tests__/functional/runTestApp.ts
+++ service/src/__tests__/functional/runTestApp.ts
@@ -1,33 +1,17 @@
-import { Server } from 'node:http';
-import { WebSocketServer } from 'ws';
-
 import { Constructor, Container } from '../../di';
 import { ConfigService } from '../../config/ConfigService';
-import { messageHandler, injectWebSocketServer, WebSocketServerModule } from '../../wss';
-import { requestHandler, HttpServerModule } from '../../http';
+import { WebSocketServerModule } from '../../wss';
+import { HttpServerModule } from '../../http';
 import { Application } from '../../Application';
 import { getAnonymousModule } from '../getAnonymousModule';
+import { runServer } from '../../runServer';
 
 const SharedModules = [HttpServerModule, WebSocketServerModule];
 
 export const runTestApp = async (modules: Constructor[]): Promise<Application> => {
   const app = new Application(new Container(), [...modules, ...SharedModules, getAnonymousModule(undefined, [ConfigService])]);
 
-  await app.run(async (container) => {
-    const server = container.resolve<Server>(Server);
-    const config: ConfigService = container.resolve<ConfigService>(ConfigService);
-    const wss = container.resolve<WebSocketServer>(WebSocketServer);
-    injectWebSocketServer(server, wss);
-
-    server.on('request', requestHandler(container));
-
-    wss.on('connection', ws => {
-      ws.on('message', messageHandler(ws, container));
-    });
-
-    server.listen(config.get('httpPort'), () => console.log(`Listening on ${config.get('httpPort')}`));
-  });
-
+  await app.run(runServer);
 
   return app;
 };
