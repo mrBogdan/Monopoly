b197e0e0b5e09b1e21ae23fcc473cbf1a1294ec8
Bohdan Babytskyi <bbabitskiy@playtika.com>
2025-04-13 20:05:27 +0300

    MPN-5: add response

---
+++ service/src/__tests__/unit/http/Response.test.ts
@@ -0,0 +1,95 @@
+import { Response } from '../../../http/Response';
+
+describe('Response', () => {
+  it('should build a response with body, headers, and status code', () => {
+    const body = { message: 'Hello, World!' };
+    const headers = { 'Content-Type': 'application/json', 'X-Custom-Header': 'CustomValue' };
+    const statusCode = 200;
+
+    const response = Response.builder()
+      .setBody(body)
+      .setHeader('Content-Type', 'application/json')
+      .setHeader('X-Custom-Header', 'CustomValue')
+      .build();
+
+    expect(response.body).toEqual(body);
+    expect(response.headers).toEqual(headers);
+    expect(response.statusCode).toEqual(statusCode);
+  });
+
+  it('should build a response with only body', () => {
+    const body = { message: 'Hello, World!' };
+
+    const response = Response.builder()
+      .setBody(body)
+      .build();
+
+    expect(response.body).toEqual(body);
+    expect(response.headers).toEqual(undefined);
+    expect(response.statusCode).toEqual(200);
+  });
+
+  it('should build a response with only headers', () => {
+    const headers = { 'Content-Type': 'application/json', 'X-Custom-Header': 'CustomValue' };
+
+    const response = Response.builder()
+      .setHeader('Content-Type', 'application/json')
+      .setHeader('X-Custom-Header', 'CustomValue')
+      .build();
+
+    expect(response.body).toEqual(undefined);
+    expect(response.headers).toEqual(headers);
+    expect(response.statusCode).toEqual(200);
+  });
+
+  it('should build a response with status code', () => {
+    const statusCode = 404;
+
+    const response = Response.builder()
+      .setStatusCode(statusCode)
+      .build();
+
+    expect(response.body).toEqual(undefined);
+    expect(response.headers).toEqual(undefined);
+    expect(response.statusCode).toEqual(statusCode);
+  });
+
+  it('should set cookies correctly', () => {
+    const response = Response.builder()
+      .setCookie('session_id', 'abc123')
+      .build();
+
+    expect(response.headers).toEqual({
+      'Set-Cookie': 'session_id=abc123',
+    });
+  });
+
+  it('should set cookies with options correctly', () => {
+    const response = Response.builder()
+      .setCookie('session_id', 'abc123', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'Strict',
+        maxAge: 3600,
+      })
+      .build();
+    expect(response.headers).toEqual({
+      'Set-Cookie': 'session_id=abc123; HttpOnly; Secure; SameSite=Strict; Max-Age=3600',
+    });
+  });
+
+  it('should handle multiple cookies correctly', () => {
+    const response = Response.builder()
+      .setHeader('Content-Type', 'application/json')
+      .setCookie('session_id', 'abc123')
+      .setCookie('user_id', 'xyz789')
+      .setStatusCode(201)
+      .setBody({ message: 'Created' })
+      .build();
+
+    expect(response.headers).toEqual({
+      'Set-Cookie': 'session_id=abc123, user_id=xyz789',
+      'Content-Type': 'application/json',
+    });
+  });
+});
---
+++ service/src/http/ResponseBuilder.ts
@@ -0,0 +1,102 @@
+import { Response } from './Response';
+import { capitalize } from '../nodejs/capitalize';
+
+interface CookieParam {
+  sameSite?: 'Strict' | 'Lax' | 'None';
+  secure?: boolean;
+  httpOnly?: boolean;
+  path?: string;
+  domain?: string;
+  expires?: Date;
+  maxAge?: number;
+}
+
+interface CookieData {
+  value: string;
+  cookieParam?: CookieParam;
+}
+
+const prepareCookieKey = (key: string): string => {
+  if (key === 'maxAge') {
+    return 'Max-Age';
+  }
+
+  return capitalize(key);
+};
+
+export class ResponseBuilder {
+  private headers: Map<string, string> = new Map<string, string>();
+  private cookies: Map<string, CookieData> = new Map<string, CookieData>();
+  private body: unknown | File | string | object | Buffer | undefined = undefined;
+  private statusCode: number = 200;
+
+  constructor() {
+  }
+
+  setBody(body: unknown): ResponseBuilder {
+    this.body = body;
+    return this;
+  }
+
+  setStatusCode(statusCode: number): ResponseBuilder {
+    this.statusCode = statusCode;
+    return this;
+  }
+
+  setHeader(name: string, value: string): ResponseBuilder {
+    this.headers.set(name, value);
+    return this;
+  }
+
+  setCookie(key: string, value: string, cookieParam?: CookieParam): ResponseBuilder {
+    this.cookies.set(key, {
+      value,
+      cookieParam,
+    });
+    return this;
+  }
+
+  build(): Response {
+    const args = [];
+
+    if (this.body) {
+      args[0] = this.body;
+    }
+
+    if (this.cookies.size) {
+      this.headers.set('Set-Cookie', Array.from(this.cookies.entries())
+        .map(([cookieKey, {value, cookieParam}]) => {
+          if (!cookieParam) {
+            return `${cookieKey}=${value}`;
+          }
+
+          const cookieOptions = Object.entries(cookieParam)
+            .filter(([, value]) => value !== undefined)
+            .map(([cookieOptionKey, cookieOptionValue]) => {
+              const isBoolean = typeof cookieOptionValue === 'boolean';
+              const httpCookieOptionKey = prepareCookieKey(cookieOptionKey);
+
+              if (isBoolean && cookieOptionValue) {
+                return httpCookieOptionKey;
+              }
+
+              return `${httpCookieOptionKey}=${cookieOptionValue}`;
+            })
+            .join('; ');
+
+          return `${cookieKey}=${value}; ${cookieOptions}`;
+        })
+        .join(', '));
+    }
+
+    if (this.headers.size) {
+      args[1] = Object.fromEntries(this.headers.entries());
+    }
+
+    if (this.statusCode) {
+      args[2] = this.statusCode;
+    }
+
+    return new Response(...args);
+  }
+}
